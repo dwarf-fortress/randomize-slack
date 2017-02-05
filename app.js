5ddb64a79bbbfcc735d44214cf84689bd9cf71ec
Sejas <antonio@sejas.es>
2017-02-05 11:20:45 +0000

    try req.body with body-parser

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var express = require('express')
+var express = require('express'),
+	bodyParser = require('body-parser')
+
 var app = express()
+
+
+var VERIFY_TOKEN = process.env.SLACK_VERIFY_TOKEN
+if (!VERIFY_TOKEN) {
+  console.error('SLACK_VERIFY_TOKEN is required')
+  process.exit(1)
+}
+
 
 app.get('/', function (req, res) {
 	res.send('<!-- !-->')
@@ -11,14 +21,16 @@ function randomize (req, res) {
 		    "text": "It's 80 degrees right now.",
 		    "attachments": [
 		        {
-		            "text":"JSON: "+JSON.stringify(req.query)
+		            "text":"JSON: "+JSON.stringify(req.body)
 		        }
 		    ]
 		}
 	res.send(answer)
 }
-app.get('/randomize', randomize)
-app.post('/randomize', randomize)
+app.route('/randomize')
+  .get(randomize)
+  .post(bodyParser.urlencoded({ extended: true }),randomize)
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
