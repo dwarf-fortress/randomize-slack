aa4343c7d941f307f58a3dc682dac78a3d9c44b2
Sejas <antonio@sejas.es>
2017-02-05 19:19:23 +0000

    response_type in_channel

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var express = require('express'),
 
 var app = express()
 
-
+//Try the command for heroku: heroku config:set SLACK_VERIFY_TOKEN=adsfasdfasdf
+//for mac os : export SLACK_VERIFY_TOKEN=adsfasdfasdf
 var VERIFY_TOKEN = process.env.SLACK_VERIFY_TOKEN
 if (!VERIFY_TOKEN) {
   console.error('SLACK_VERIFY_TOKEN is required')
+  console.log('Try the command for heroku: heroku config:set SLACK_VERIFY_TOKEN=adsfasdfasdf')
   process.exit(1)
 }
 
@@ -22,6 +24,7 @@ app.get('/', function (req, res) {
 // RANDOMIZE
 function randomize (req, res) {
 	var answer = {
+			"response_type": "in_channel",
 		    "text": "Smultron ha hablado. El orden random es:",
 		    "attachments": [
 		        {
